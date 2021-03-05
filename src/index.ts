e4f2466ca43f87829b0fd80723a6f6ad66475c07
Qolim <liming12679@126.com>
2021-03-06 03:39:19 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: LimingQi
+ * @Date: 2021-03-06 03:39:04
+ * @LastEditTime: 2021-03-06 03:39:08
+ * @LastEditors: LimingQi
+ * @Description:入口文件
+ * @FilePath: /admin-hooks/src/index.ts
+ * Github: https://github.com/Qolim
+ */
+export {
+
+}
